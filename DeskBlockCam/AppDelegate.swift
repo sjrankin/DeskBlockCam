2d2f2c83f13b026056b892f2c61a5ba976b0cce6
Stuart Rankin <sjrankin@gmail.com>
2020-02-02 15:45:10 +0900

    Initial Commit

---
+++ DeskBlockCam/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DeskBlockCam
+//
+//  Created by Stuart Rankin on 2/2/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DeskBlockCam/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DeskBlockCam
+//
+//  Created by Stuart Rankin on 2/2/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
