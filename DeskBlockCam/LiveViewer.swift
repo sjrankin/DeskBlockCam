4fbcc530a97c8e7dc8853882f9e9a9b6706a527e
Stuart Rankin <sjrankin@gmail.com>
2020-02-05 17:49:02 +0900

    Create LiveViewer.swift
    Placeholder.

---
+++ DeskBlockCam/LiveViewer.swift
@@ -0,0 +1,14 @@
+//
+//  LiveViewer.swift
+//  DeskBlockCam
+//
+//  Created by Stuart Rankin on 2/2/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class LiveViewer: NSView
+{
+}
