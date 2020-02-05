5c7eecb87dc62137b4963834e118c694f8d1b9d6
Stuart Rankin <sjrankin@gmail.com>
2020-02-05 17:50:47 +0900

    Update AppDelegate.swift
    Code formatted. Added applicationShouldTerminateAfterLastWindowClosed.

--- DeskBlockCam/AppDelegate.swift
+++ DeskBlockCam/AppDelegate.swift
@@ -8,19 +8,22 @@
 
 import Cocoa
 
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
+@NSApplicationMain class AppDelegate: NSObject, NSApplicationDelegate
+{
+    func applicationDidFinishLaunching(_ aNotification: Notification)
+    {
         // Insert code here to initialize your application
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
+    func applicationWillTerminate(_ aNotification: Notification)
+    {
         // Insert code here to tear down your application
     }
-
-
+    
+    // Don't let orphan windows stop the app from terminating.
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool
+    {
+        return true
+    }
 }
 
