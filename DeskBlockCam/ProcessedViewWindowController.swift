d6fd8cdfe81b7e10501ad160dcb0281df2284764
Stuart Rankin <sjrankin@gmail.com>
2020-02-05 17:53:29 +0900

    Create ProcessedViewWindowController.swift
    Window controller for the processed view controller.

---
+++ DeskBlockCam/ProcessedViewWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  ProcessedViewWindowController.swift
+//  DeskBlockCam
+//
+//  Created by Stuart Rankin on 2/3/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ProcessedViewWindowController: NSWindowController
+{
+    /// Close the window.
+    func CloseWindow()
+    {
+        self.close()
+    }
+}
