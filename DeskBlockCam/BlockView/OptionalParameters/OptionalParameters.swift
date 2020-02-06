a9be866fb46fa75cd2559232483881cc6897569e
Stuart Rankin <sjrankin@gmail.com>
2020-02-06 17:06:54 +0900

    Create OptionalParameters.swift
    Parent class for all optional parameters.

---
+++ DeskBlockCam/BlockView/OptionalParameters/OptionalParameters.swift
@@ -0,0 +1,27 @@
+//
+//  OptionalParameters.swift
+//  DeskBlockCam
+//
+//  Created by Stuart Rankin on 2/6/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class OptionalParameters
+{
+    init(WithShape: Shapes)
+    {
+        _ForShape = WithShape
+    }
+    
+    private var _ForShape: Shapes = .Blocks
+    public var ForShape: Shapes
+    {
+        get
+        {
+            return _ForShape
+        }
+    }
+}
