33c316c3947b8d76d9f69c5c25ae196232124208
Stuart Rankin <sjrankin@gmail.com>
2020-02-07 20:30:40 +0900

    Create OvalOptionsCode.swift
    Code to run the oval options UI.

---
+++ DeskBlockCam/Settings/ShapeDialogs/OvalOptionsCode.swift
@@ -0,0 +1,41 @@
+//
+//  OvalOptionsCode.swift
+//  DeskBlockCam
+//
+//  Created by Stuart Rankin on 2/7/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class OvalOptionsCode: NSViewController, ToOptionsDialogProtocol
+{
+    weak var Delegate: ToOptionsParentProtocol? = nil
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        if !NewCaption.isEmpty
+        {
+        Caption.stringValue = NewCaption
+        }
+    }
+    
+    var NewCaption: String = ""
+    
+    func SetAttributes(_ Attributes: ProcessingAttributes)
+    {
+    }
+    
+    func SetCaption(_ CaptionText: String)
+    {
+        NewCaption = CaptionText
+        if Caption != nil
+        {
+            Caption.stringValue = NewCaption
+        }
+    }
+    
+    @IBOutlet weak var Caption: NSTextField!
+}
