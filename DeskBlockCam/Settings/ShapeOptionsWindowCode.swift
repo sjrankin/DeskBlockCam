c38b52b8901dda2a869a90061305c6ee763533c9
Stuart Rankin <sjrankin@gmail.com>
2020-02-07 20:29:10 +0900

    Create ShapeOptionsWindowCode.swift
    Code to run the settings UI window.

---
+++ DeskBlockCam/Settings/ShapeOptionsWindowCode.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeOptionsWindowCode.swift
+//  DeskBlockCam
+//
+//  Created by Stuart Rankin on 2/6/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ShapeOptionsWindowCode: NSWindowController
+{
+    func CloseWindow()
+    {
+        self.close()
+    }
+}
