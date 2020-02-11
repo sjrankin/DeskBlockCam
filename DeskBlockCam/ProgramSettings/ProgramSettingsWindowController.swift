f2e789266763bb812f970fbc17dfeabbd11a2fac
Stuart Rankin <sjrankin@gmail.com>
2020-02-11 18:24:36 +0900

    Create ProgramSettingsWindowController.swift
    Program settings window code.

---
+++ DeskBlockCam/ProgramSettings/ProgramSettingsWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  ProgramSettingsWindowController.swift
+//  DeskBlockCam
+//
+//  Created by Stuart Rankin on 2/10/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ProgramSettingsWindowController: NSWindowController
+{
+    func CloseWindow()
+    {
+        self.close()
+    }
+}
