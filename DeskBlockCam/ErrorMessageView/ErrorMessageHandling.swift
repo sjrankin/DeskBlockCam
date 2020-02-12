8bf2289051ba4fd8f883dfd97067a53edce829ce
Stuart Rankin <sjrankin@gmail.com>
2020-02-12 17:43:47 +0900

    Create ErrorMessageHandling.swift
    Extension for the main view to handle error messages.

---
+++ DeskBlockCam/ErrorMessageView/ErrorMessageHandling.swift
@@ -0,0 +1,31 @@
+//
+//  ErrorMessageHandling.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 2/12/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+extension ViewController
+{
+    /// Show an error message in a pop-up window.
+    /// - Parameter Message: The text of the message to display.
+    /// - Parameter WindowTitle: The title of the window. If nil, "Error" will be shown.
+    func ShowError(Message: String, WindowTitle: String? = nil)
+    {
+        print("\(WindowTitle ?? "Error"): \(Message)")
+        let Storyboard = NSStoryboard(name: "ErrorMessage", bundle: nil)
+        if let WindowController = Storyboard.instantiateController(withIdentifier: "ErrorMessageWindowID") as? ErrorMessageWindowCode
+        {
+            if let ErrorTitle = WindowTitle
+            {
+                WindowController.SetTitle(NewTitle: ErrorTitle)
+            }
+            WindowController.showWindow(nil)
+            WindowController.SetErrorMessage(Message)
+        }
+    }
+}
