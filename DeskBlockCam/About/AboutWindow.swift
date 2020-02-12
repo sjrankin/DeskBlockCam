ffdcee86be1e1243a0622bef80b405505ad8dfc0
Stuart Rankin <sjrankin@gmail.com>
2020-02-12 17:46:21 +0900

    Create AboutWindow.swift
    Code to run the about window.

---
+++ DeskBlockCam/About/AboutWindow.swift
@@ -0,0 +1,18 @@
+//
+//  AboutWindow.swift
+//  DeskBlockCam
+//
+//  Created by Stuart Rankin on 2/12/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class AboutWindow: NSWindowController
+{
+    func CloseWindow()
+    {
+        self.close()
+    }
+}
