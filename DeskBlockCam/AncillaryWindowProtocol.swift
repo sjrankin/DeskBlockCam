e355fa37a9af61e9f470b8c1a88b1b97d18d3c4d
Stuart Rankin <sjrankin@gmail.com>
2020-02-12 17:45:57 +0900

    Create AncillaryWindowProtocol.swift
    Protocol to tell subscribers that a window closed.

---
+++ DeskBlockCam/AncillaryWindowProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  AncillaryWindowProtocol.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 2/12/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+protocol AncillaryWindowProtocol: class
+{
+    func ClosedAncillaryWindow(WindowName: String)
+}
