df77f9df90fce02ab6fdb5eb0bafae62ff88d263
Stuart Rankin <sjrankin@gmail.com>
2020-02-14 22:49:54 +0900

    Create SphereOptionCode.swift
    Code to run the sphere options UI.

---
+++ DeskBlockCam/Settings/ShapeDialogs/SphereOptionCode.swift
@@ -0,0 +1,46 @@
+//
+//  SphereOptionCode.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 2/14/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class SphereOptionCode: NSViewController, ToOptionsParentProtocol
+{
+    weak var Delegate: ToOptionsParentProtocol? = nil
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        if !NewCaption.isEmpty
+        {
+            Caption.stringValue = NewCaption
+        }
+    }
+    
+    var NewCaption: String = ""
+    
+    func SetAttributes(_ Attributes: ProcessingAttributes)
+    {
+    }
+    
+    func UpdatedOptions(_ Updated: ProcessingAttributes)
+    {
+        
+    }
+    
+    func SetCaption(_ CaptionText: String)
+    {
+        NewCaption = CaptionText
+        if Caption != nil
+        {
+            Caption.stringValue = NewCaption
+        }
+    }
+    
+    @IBOutlet weak var Caption: NSTextField!
+}
