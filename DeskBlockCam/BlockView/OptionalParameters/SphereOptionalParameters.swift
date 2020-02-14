a162dd295175a0e081dfad0126a4682af4cb4e69
Stuart Rankin <sjrankin@gmail.com>
2020-02-14 22:50:07 +0900

    Create SphereOptionalParameters.swift
    Sphere optional parameters class.

---
+++ DeskBlockCam/BlockView/OptionalParameters/SphereOptionalParameters.swift
@@ -0,0 +1,49 @@
+//
+//  SphereOptionalParameters.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 2/14/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class SphereOptionalParameters: OptionalParameters
+{
+    init()
+    {
+        super.init(WithShape: .Spheres)
+        self.Read()
+    }
+    
+    init(WithOrientation: Orientations, Behavior: SphereBehaviors)
+    {
+        super.init(WithShape: .Spheres)
+        _Behavior = Behavior
+    }
+    
+    private var _Behavior: SphereBehaviors = .Size
+    public var Behavior: SphereBehaviors
+    {
+        get
+        {
+            return _Behavior
+        }
+        set
+        {
+            _Behavior = newValue
+        }
+    }
+    
+    override public func Read()
+    {
+        _Behavior = Settings.GetEnum(ForKey: .SphereBehavior, EnumType: SphereBehaviors.self, Default: SphereBehaviors.Size)
+    }
+    
+    override public func Write()
+    {
+        Settings.SetEnum(Behavior, EnumType: SphereBehaviors.self, ForKey: .SphereBehavior)
+    }
+}
+
