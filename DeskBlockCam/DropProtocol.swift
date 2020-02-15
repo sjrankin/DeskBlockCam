3120cdf72483bf1921b950e7d909b48a7dfa7e23
Stuart Rankin <sjrankin@gmail.com>
2020-02-15 22:03:52 +0900

    Create DropProtocol.swift
    Recovered accidentally. Will delete at a later date.

---
+++ DeskBlockCam/DropProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  DropProtocol.swift
+//  DeskBlockCam
+//
+//  Created by Stuart Rankin on 2/10/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+protocol DropProtocol: class
+{
+    func DroppedFile(FileName: String)
+}
