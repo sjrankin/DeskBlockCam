7bb9c5706e28c525359865d8b6229545b8ec7a96
Stuart Rankin <sjrankin@gmail.com>
2020-02-15 22:02:33 +0900

    Create RingOptionCode.swift
    Added code to run ring options UI.

---
+++ DeskBlockCam/Settings/ShapeDialogs/RingOptionCode.swift
@@ -0,0 +1,115 @@
+//
+//  RingOptionCode.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 2/15/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class RingOptionCode: NSViewController, ToOptionsParentProtocol
+{
+    weak var Delegate: ToOptionsParentProtocol? = nil
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        if !NewCaption.isEmpty
+        {
+            Caption.stringValue = NewCaption
+        }
+    }
+    
+    var NewCaption: String = ""
+    
+    func ApplyAttributes(_ NewAttributes: ProcessingAttributes)
+    {
+        if let RingAttributes = NewAttributes.GetOptionsFor(.Rings) as? RingOptionalParameters
+        {
+            switch RingAttributes.DonutHoleSize
+            {
+                case .Small:
+                    DonutHoleSegment.selectedSegment = 0
+                
+                case .Medium:
+                    DonutHoleSegment.selectedSegment = 1
+                
+                case .Large:
+                    DonutHoleSegment.selectedSegment = 2
+            }
+            switch RingAttributes.Orientation
+            {
+                case .Flat:
+                    OrientationSegment.selectedSegment = 0
+                
+                case .Rotated:
+                    OrientationSegment.selectedSegment = 1
+            }
+        }
+    }
+    
+    func SetAttributes(_ Attributes: ProcessingAttributes)
+    {
+        ApplyAttributes(Attributes)
+    }
+    
+    func UpdatedOptions(_ Updated: ProcessingAttributes)
+    {
+        ApplyAttributes(Updated)
+    }
+    
+    func SetCaption(_ CaptionText: String)
+    {
+        NewCaption = CaptionText
+        if Caption != nil
+        {
+            Caption.stringValue = NewCaption
+        }
+    }
+    
+    @IBAction func HandleRingOrientationChanged(_ sender: Any)
+    {
+        if let Segment = sender as? NSSegmentedControl
+        {
+            switch Segment.selectedSegment
+            {
+                case 0:
+                    Settings.SetEnum(RingOrientations.Flat, EnumType: RingOrientations.self, ForKey: .RingOrientation)
+                
+                case 1:
+                    Settings.SetEnum(RingOrientations.Rotated, EnumType: RingOrientations.self, ForKey: .RingOrientation)
+                
+                default:
+                    Settings.SetEnum(RingOrientations.Flat, EnumType: RingOrientations.self, ForKey: .RingOrientation)
+            }
+        }
+    }
+    
+    @IBAction func HandleDonutHoleSizeChanged(_ sender: Any)
+    {
+        if let Segment = sender as? NSSegmentedControl
+        {
+            switch Segment.selectedSegment
+            {
+                case 0:
+                    Settings.SetEnum(DonutHoleSizes.Small, EnumType: DonutHoleSizes.self, ForKey: .DonutHoleSize)
+                
+                case 1:
+                    Settings.SetEnum(DonutHoleSizes.Medium, EnumType: DonutHoleSizes.self, ForKey: .DonutHoleSize)
+                
+                case 2:
+                    Settings.SetEnum(DonutHoleSizes.Large, EnumType: DonutHoleSizes.self, ForKey: .DonutHoleSize)
+                
+                default:
+                    Settings.SetEnum(DonutHoleSizes.Medium, EnumType: DonutHoleSizes.self, ForKey: .RingOrientation)
+            }
+        }
+    }
+    
+    @IBOutlet weak var DonutHoleSegment: NSSegmentedControl!
+    @IBOutlet weak var OrientationSegment: NSSegmentedControl!
+    @IBOutlet weak var Caption: NSTextField!
+}
+
