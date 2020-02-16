1ded563c91964400c89623c3128e52e3c223a745
Stuart Rankin <sjrankin@gmail.com>
2020-02-16 22:31:25 +0900

    Create ShapeOptionsCode2.swift
    Placeholder for next version of shape options dialog.

---
+++ DeskBlockCam/Settings/ShapeOptionsCode2.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeOptionsCode2.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 2/16/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ShapeOptionsCode2: NSSplitViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
