0f90a8ef493d362d3a7be0c5527803032f8f9175
Stuart Rankin <sjrankin@gmail.com>
2020-02-18 10:41:39 +0900

    Update CharacterSetOptionsCode.swift
    Connected UI to code.

--- DeskBlockCam/Settings/ShapeDialogs/CharacterSetOptionsCode.swift
+++ DeskBlockCam/Settings/ShapeDialogs/CharacterSetOptionsCode.swift
@@ -20,6 +20,24 @@ class CharacterSetOptionsCode: NSViewController, ToOptionsDialogProtocol
         {
             Caption.stringValue = NewCaption
         }
+        InitializeUI()
+    }
+    
+    func InitializeUI()
+    {
+        CharacterCombo.removeAllItems()
+        let AllSets = CharacterSets.allCases.sorted
+        {
+            $0.rawValue < $1.rawValue
+        }
+        for CSet in AllSets
+        {
+            CharacterCombo.addItem(withObjectValue: CSet.rawValue)
+        }
+        let Current = Settings.GetEnum(ForKey: .CharacterSet, EnumType: CharacterSets.self,
+                                       Default: CharacterSets.Latin)
+        CharacterCombo.selectItem(withObjectValue: Current.rawValue)
+        ShowCharactersFor(Current)
     }
     
     var NewCaption: String = ""
@@ -42,7 +60,29 @@ class CharacterSetOptionsCode: NSViewController, ToOptionsDialogProtocol
         CurrentShape = Shape
     }
     
+    func ShowCharactersFor(_ SomeSet: CharacterSets)
+    {
+        let (CharList, FontName) = ShapeManager.GetCharacterSetInfo(SomeSet)
+        CharactersView.font = NSFont(name: FontName, size: 14.0)
+        var Final = CharList.trimmingCharacters(in: CharacterSet.whitespaces)
+        Final = Final.replacingOccurrences(of: "\n", with: "")
+        Final = Final.replacingOccurrences(of: " ", with: "")
+        CharactersView.string = Final
+        CharBox.title = "Characters in set for font \(FontName)"
+    }
+    
+    @IBAction func HandleCharacterSetChanged(_ sender: Any)
+    {
+        if let Selected = CharacterCombo.objectValueOfSelectedItem as? String
+        {
+            ShowCharactersFor(CharacterSets(rawValue: Selected)!)
+        }
+    }
+    
     var CurrentShape: Shapes = .NoShape
-
+    
+    @IBOutlet weak var CharBox: NSBox!
+    @IBOutlet var CharactersView: NSTextView!
+    @IBOutlet weak var CharacterCombo: NSComboBox!
     @IBOutlet weak var Caption: NSTextField!
 }
