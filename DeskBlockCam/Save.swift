3f5334a9602dd36e8aefaad696ed86d9f2207875
Stuart Rankin <sjrankin@gmail.com>
2020-02-18 12:55:08 +0900

    Create Save.swift
    Code that runs NSSavePanel to get a location to save images.

---
+++ DeskBlockCam/Save.swift
@@ -0,0 +1,35 @@
+//
+//  Save.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 2/18/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+extension ViewController
+{
+    /// Returns the URL of a file. This is where the user wants to save an image.
+    /// - Returns: URL of where to save the file. Nil if the user canceled.
+    func GetSaveImageLocation() -> URL?
+    {
+        let SavePanel = NSSavePanel()
+        SavePanel.showsTagField = false
+        SavePanel.title = "Save Image"
+        SavePanel.allowedFileTypes = ["jpg", "jpeg", "png", "tiff"]
+        SavePanel.canCreateDirectories = true
+        SavePanel.nameFieldStringValue = "BlockCamImage.jpg"
+        SavePanel.level = .modalPanel
+        if SavePanel.runModal() == .OK
+        {
+            return SavePanel.url
+        }
+        else
+        {
+            return nil
+        }
+    }
+}
+
