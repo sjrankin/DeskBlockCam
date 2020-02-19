c87065a3e32140fc4c80c1b03882d713722b735f
Stuart Rankin <sjrankin@gmail.com>
2020-02-19 15:25:33 +0900

    Update ProgramSettingsCode.swift
    Added privacy tab code for new UI.

--- DeskBlockCam/ProgramSettings/ProgramSettingsCode.swift
+++ DeskBlockCam/ProgramSettings/ProgramSettingsCode.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 import AppKit
+import Cocoa
 
-class ProgramSettingsCode: NSViewController
+class ProgramSettingsCode: NSViewController, NSTextFieldDelegate
 {
     override func viewDidLoad()
     {
@@ -17,6 +18,43 @@ class ProgramSettingsCode: NSViewController
         ShowHistogramCheck.state = Settings.GetBoolean(ForKey: .ShowHistogram) ? .on : .off
         OpenShapeOptionsCheck.state = Settings.GetBoolean(ForKey: .AutoOpenShapeSettings) ? .on : .off
         SwitchOnDropCheck.state = Settings.GetBoolean(ForKey: .SwitchModesWithDroppedImages) ? .on : .off
+        InitializePrivacyTab()
+    }
+    
+    func InitializePrivacyTab()
+    {
+        CopyrightTextBox.delegate = self
+        CopyrightTextBox.stringValue = Settings.GetString(ForKey: .UserCopyright) ?? ""
+        NameTextBox.delegate = self
+        NameTextBox.stringValue = Settings.GetString(ForKey: .UserName) ?? ""
+        SaveMetadataCheck.state = Settings.GetBoolean(ForKey: .AddUserDataToExif) ? .on : .off
+        IncludeCopyrightCheck.state = Settings.GetBoolean(ForKey: .SaveUserCopyright) ? .on : .off
+        IncludeNameCheck.state = Settings.GetBoolean(ForKey: .SaveUserName) ? .on : .off
+        UpdateUI()
+    }
+    
+    func UpdateUI()
+    {
+        if Settings.GetBoolean(ForKey: .SaveUserCopyright)
+        {
+            CopyrightTextBox.stringValue = Settings.GetString(ForKey: .UserCopyright) ?? ""
+            CopyrightTextBox.isEnabled = true
+        }
+        else
+        {
+            CopyrightTextBox.stringValue = ""
+            CopyrightTextBox.isEnabled = false
+        }
+        if Settings.GetBoolean(ForKey: .SaveUserName)
+        {
+            NameTextBox.stringValue = Settings.GetString(ForKey: .UserName) ?? ""
+            NameTextBox.isEnabled = true
+        }
+        else
+        {
+            NameTextBox.stringValue = ""
+            NameTextBox.isEnabled = false
+        }
     }
     
     @IBAction func HandleCloseButton(_ sender: Any)
@@ -48,6 +86,55 @@ class ProgramSettingsCode: NSViewController
         }
     }
     
+    @IBAction func HandleNameCheckChanged(_ sender: Any)
+    {
+        if let Check = sender as? NSButton
+        {
+            Settings.SetBoolean(Check.state == .on, ForKey: .SaveUserName)
+            UpdateUI()
+        }
+    }
+    
+    @IBAction func HandleCopyrightCheckChanged(_ sender: Any)
+    {
+        if let Check = sender as? NSButton
+        {
+            Settings.SetBoolean(Check.state == .on, ForKey: .SaveUserCopyright)
+            UpdateUI()
+        }
+    }
+    
+    @IBAction func HandleSaveMetadataChanged(_ sender: Any)
+    {
+        if let Check = sender as? NSButton
+        {
+            Settings.SetBoolean(Check.state == .on, ForKey: .AddUserDataToExif)
+        }
+    }
+    
+     func controlTextDidChange(_ notification: Notification)
+    {
+        if let Field = notification.object as? NSTextField
+        {
+            switch Field
+            {
+                case CopyrightTextBox:
+                    Settings.SetString(CopyrightTextBox.stringValue, ForKey: .UserCopyright)
+                
+                case NameTextBox:
+                    Settings.SetString(NameTextBox.stringValue, ForKey: .UserName)
+                
+                default:
+                break
+            }
+        }
+    }
+    
+    @IBOutlet weak var CopyrightTextBox: NSTextField!
+    @IBOutlet weak var NameTextBox: NSTextField!
+    @IBOutlet weak var IncludeCopyrightCheck: NSButton!
+    @IBOutlet weak var IncludeNameCheck: NSButton!
+    @IBOutlet weak var SaveMetadataCheck: NSButton!
     @IBOutlet weak var SwitchOnDropCheck: NSButton!
     @IBOutlet weak var ShowHistogramCheck: NSButton!
     @IBOutlet weak var OpenShapeOptionsCheck: NSButton!
