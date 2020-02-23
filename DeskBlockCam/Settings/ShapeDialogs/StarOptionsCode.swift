ab90b5c7d5c7515e4a7ff5b5f941dd09e92d9890
Stuart Rankin <sjrankin@gmail.com>
2020-02-23 18:10:48 +0900

    Update StarOptionsCode.swift
    Added sample.

--- DeskBlockCam/Settings/ShapeDialogs/StarOptionsCode.swift
+++ DeskBlockCam/Settings/ShapeDialogs/StarOptionsCode.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import AppKit
+import SceneKit
 
 class StarOptionsCode: NSViewController, ToOptionsDialogProtocol
 {
@@ -24,6 +25,7 @@ class StarOptionsCode: NSViewController, ToOptionsDialogProtocol
         let Apexes = Settings.GetInteger(ForKey: .StarApexCount)
         ApexSegment.selectedSegment = Apexes - 4
         VariableApexesCheck.state = Varies ? .on : .off
+        ShowSample(Apexes)
     }
     
     var NewCaption: String = ""
@@ -61,10 +63,50 @@ class StarOptionsCode: NSViewController, ToOptionsDialogProtocol
         let Index = ApexSegment.selectedSegment + 4
         Settings.SetInteger(Index, ForKey: .StarApexCount)
         Delegate?.UpdateCurrent(With: CurrentShape)
+        ShowSample(Index)
     }
+    
+        var SampleInitialized = false
+    
+    func ShowSample(_ ApexCount: Int)
+    {
+        if !SampleInitialized
+        {
+            SampleInitialized = true
+            StarSample.scene = SCNScene()
+            StarSample.scene?.background.contents = NSColor.black
+            let Light = SCNLight()
+            Light.color = NSColor.white
+            Light.type = .omni
+            let LightNode = SCNNode()
+            LightNode.light = Light
+            LightNode.position = SCNVector3(-4.0, 4.0, 15.0)
+            StarSample.scene?.rootNode.addChildNode(LightNode)
+            let Camera = SCNCamera()
+            Camera.fieldOfView = 90.0
+            let CameraNode = SCNNode()
+            CameraNode.camera = Camera
+            CameraNode.position = SCNVector3(1.0, 1.0, 15.0)
+            StarSample.scene?.rootNode.addChildNode(CameraNode)
+        }
+        if SampleStar != nil
+        {
+            SampleStar?.removeFromParentNode()
+            SampleStar = nil
+        }
+  
+        SampleStar = SCNStar(VertexCount: ApexCount, Height: 10.0, Base: 5.0, ZHeight: 5.0)
+        SampleStar?.geometry?.firstMaterial?.lightingModel = .lambert
+        SampleStar?.geometry?.firstMaterial?.diffuse.contents = NSColor.systemYellow
+        SampleStar?.geometry?.firstMaterial?.specular.contents = NSColor.white
+        StarSample.scene?.rootNode.addChildNode(SampleStar!)
+    }
+    
+    var SampleStar: SCNStar? = nil
     
     var CurrentShape: Shapes = .NoShape
     
+    @IBOutlet weak var StarSample: SCNView!
     @IBOutlet weak var VariableApexesCheck: NSButton!
     @IBOutlet weak var ApexSegment: NSSegmentedControl!
     @IBOutlet weak var Caption: NSTextField!
