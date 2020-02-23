4db3450ef6441ec5576c57827fa9b47cb4572f68
Stuart Rankin <sjrankin@gmail.com>
2020-02-23 10:05:51 +0900

    Create RedrawProtocol.swift
    Protocol to request redrawing a processed image from a dialog box.

---
+++ DeskBlockCam/RedrawProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  RedrawProtocol.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 2/22/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+
+protocol RedrawProtocol: class
+{
+    func RedrawImage()
+}
