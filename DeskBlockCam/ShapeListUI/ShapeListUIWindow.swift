5971421930fd0d9e2651b885f675262db788924e
Stuart Rankin <sjrankin@gmail.com>
2020-02-24 18:06:14 +0900

    Create ShapeListUIWindow.swift
    Shape list UI window code.

---
+++ DeskBlockCam/ShapeListUI/ShapeListUIWindow.swift
@@ -0,0 +1,18 @@
+//
+//  ShapeListUIWindow.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 2/24/20.
+//  Copyright © 2020 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class ShapeListUIWindow: NSWindowController
+{
+    func CloseWindow()
+    {
+        self.close()
+    }
+}
