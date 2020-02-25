a523242643682e2456ddac4c484f60f42574391e
Stuart Rankin <sjrankin@gmail.com>
2020-02-25 20:48:57 +0900

    Update Versioning.swift
    Auto updated.

--- DeskBlockCam/Versioning.swift
+++ DeskBlockCam/Versioning.swift
@@ -114,19 +114,19 @@ public class Versioning: CustomStringConvertible
     }
     
     /// Build number.
-    public static let Build: Int = 403
+    public static let Build: Int = 408
     
     /// Build increment.
     private static let BuildIncrement = 1
     
     /// Build ID.
-    public static let BuildID: String = "7E233682-79A0-4533-A7BE-B72E9B1079CE"
+    public static let BuildID: String = "26495E3A-EFB2-40BF-854F-1A09434F5C51"
     
     /// Build date.
     public static let BuildDate: String = "25 February 2020"
     
     /// Build Time.
-    public static let BuildTime: String = "11:23"
+    public static let BuildTime: String = "18:03"
     
     /// Holds the release build flag.
     private static var _IsReleaseBuild: Bool = false
