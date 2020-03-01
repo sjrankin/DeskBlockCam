08149611d31639f6e7a9e89ad87e3644af1d8179
Stuart Rankin <sjrankin@gmail.com>
2020-03-01 11:21:36 +0900

    Update Generator2D.swift
    Added code to control the height and size of 2D shapes.

--- DeskBlockCam/Generator/GeneratorExtensions/Generator2D.swift
+++ DeskBlockCam/Generator/GeneratorExtensions/Generator2D.swift
@@ -16,26 +16,65 @@ extension Generator
                                    ZLocation: inout CGFloat) -> PSCNNode?
     {
         var Node = PSCNNode()
+        var ColorUse = ColorControls.Height
+        if let (_, ColorControl) = Settings.Get2DAxisAndColorInfo(Shape)
+        {
+            ColorUse = ColorControl
+        }
+        let FinalHeight = ColorUse == .Size ? Height + 1.0 : Height
+        var Geo = SCNGeometry()
         switch Shape
         {
             case .Squares:
-                let Geo = SCNBox(width: Side * 1.5, height: Side * 1.5, length: 0.05, chamferRadius: 0.0)
+                if ColorUse == .Height
+                {
+                    Geo = SCNBox(width: Side * 1.5, height: Side * 1.5, length: 0.05, chamferRadius: 0.0)
+                }
+                else
+                {
+                    Geo = SCNBox(width: Side * FinalHeight, height: Side * FinalHeight, length: 0.05, chamferRadius: 0.0)
+                }
                 Node = PSCNNode(geometry: Geo, X: AtX, Y: AtY)
             
             case .Rectangles:
-                let Geo = SCNBox(width: Side * 1.5, height: Side * 0.75, length: 0.05, chamferRadius: 0.0)
+                if ColorUse == .Height
+                {
+                    Geo = SCNBox(width: Side * 1.5, height: Side * 0.75, length: 0.05, chamferRadius: 0.0)
+                }
+                else
+                {
+                    Geo = SCNBox(width: Side * FinalHeight, height: Side * FinalHeight * 0.75, length: 0.05, chamferRadius: 0.0)
+                }
                 Node = PSCNNode(geometry: Geo, X: AtX, Y: AtY)
             
             case .Circles:
-                let Geo = SCNCylinder(radius: Side * 0.85, height: 0.05)
+                if ColorUse == .Height
+                {
+                    Geo = SCNCylinder(radius: Side * 0.85, height: 0.05)
+                }
+                else
+                {
+                    Geo = SCNCylinder(radius: Side * FinalHeight, height: 0.05)
+                }
                 Node = PSCNNode(geometry: Geo, X: AtX, Y: AtY)
             
             case .Triangles2D:
-                let Geo = SCNTriangle.Geometry(A: 0.05, B: 0.05, C: 0.05, Scale: 1.0)
+                if ColorUse == .Height
+                {
+                    Geo = SCNTriangle.Geometry(A: 0.05, B: 0.05, C: 0.05, Scale: 1.0)
+                }
+                else
+                {
+                    Geo = SCNTriangle.Geometry(A: 0.05, B: 0.05, C: 0.05, Scale: Float(FinalHeight * 1.5))
+                }
                 Node = PSCNNode(geometry: Geo, X: AtX, Y: AtY)
             
             case .Stars2D:
-                let Dim = Double(Side * 1.5)
+                var Dim = Double(Side * 1.5)
+                if ColorUse == .Size
+                {
+                    Dim = Double(FinalHeight * 1.5)
+                }
                 var ApexCount = Settings.GetInteger(ForKey: .StarApexCount)
                 if Settings.GetBoolean(ForKey: .ApexesIncrease)
                 {
@@ -45,30 +84,54 @@ extension Generator
                         ApexCount = 12
                     }
                 }
-                let Geo = SCNStar.Geometry(VertexCount: ApexCount, Height: Dim, Base: Dim * 0.5, ZHeight: 0.05)
+                Geo = SCNStar.Geometry(VertexCount: ApexCount, Height: Dim, Base: Dim * 0.5, ZHeight: 0.05)
                 Node = PSCNNode(geometry: Geo, X: AtX, Y: AtY)
             
             case .Polygons2D:
                 let SideCount = Settings.GetInteger(ForKey: .PolygonSideCount)
-                let NGon = SCNPolygon.Geometry(VertexCount: SideCount, Radius: Side / 2.0, Depth: 0.05)
-                Node = PSCNNode(geometry: NGon, X: AtX, Y: AtY)
+                if ColorUse == .Height
+                {
+                    Geo = SCNPolygon.Geometry(VertexCount: SideCount, Radius: Side / 2.0, Depth: 0.05)
+                }
+                else
+                {
+                    Geo = SCNPolygon.Geometry(VertexCount: SideCount, Radius: FinalHeight / 2.0, Depth: 0.05)
+                }
+                Node = PSCNNode(geometry: Geo, X: AtX, Y: AtY)
             
             case .Oval2D:
-                let (Major, Minor) = GetOvalParameters(ShapeKey: .OvalLength)
-                let Geo = SCNEllipse.Geometry(MajorAxis: Major, MinorAxis: Minor, Height: 0.05)
+                var (Major, Minor) = GetOvalParameters(ShapeKey: .OvalLength)
+                if ColorUse == .Size
+                {
+                    Major = Major * FinalHeight
+                    Minor = Minor * FinalHeight
+                }
+                Geo = SCNEllipse.Geometry(MajorAxis: Major, MinorAxis: Minor, Height: 0.05)
                 Node = PSCNNode(geometry: Geo, X: AtX, Y: AtY)
                 Node.scale = SCNEllipse.ReciprocalScale()
             
             case .Diamond2D:
-                let (Major, Minor) = GetOvalParameters(ShapeKey: .DiamondLength)
-                let Geo = SCNDiamond.Geometry(MajorAxis: Major, MinorAxis: Minor, Height: 0.05)
+                var (Major, Minor) = GetOvalParameters(ShapeKey: .DiamondLength)
+                if ColorUse == .Size
+                {
+                    Major = Major * FinalHeight
+                    Minor = Minor * FinalHeight
+                }
+                Geo = SCNDiamond.Geometry(MajorAxis: Major, MinorAxis: Minor, Height: 0.05)
                 Node = PSCNNode(geometry: Geo, X: AtX, Y: AtY)
             
             default:
                 return nil
         }
         
-        ZLocation = Height * 2.0
+        if ColorUse == .Height
+        {
+            ZLocation = Height * 2.0
+        }
+        else
+        {
+            ZLocation = Height * 0.1
+        }
         Node.geometry?.firstMaterial?.diffuse.contents = Color
         Node.geometry?.firstMaterial?.specular.contents = NSColor.white
         Node.geometry?.firstMaterial?.lightingModel = GetLightModel()
