3b37eb46cb73f07fb660b7ea76b362a1d6920349
Stuart Rankin <sjrankin@gmail.com>
2021-02-14 12:52:44 +0900

    Create BlockInfoParameters.swift
    Parameters used by PixelCounter.metal.

---
+++ DeskBlockCam/Metal/BlockInfoParameters.swift
@@ -0,0 +1,19 @@
+//
+//  BlockInfoParameters.swift
+//  BlockCam
+//
+//  Created by Stuart Rankin on 2/12/21.
+//  Copyright © 2021 Stuart Rankin. All rights reserved.
+//
+
+import Foundation
+import simd
+import AppKit
+
+struct BlockInfoParameters
+{
+    let Width: simd.uint
+    let BlockWidth: simd.uint
+    let Height: simd.uint
+}
+
